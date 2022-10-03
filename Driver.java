7017fef837797415da1fbffd89c65cf70b7a7e80
Roger Zhao <you@example.comrzhao1st@gmail.com>
2022-10-03 12:46:14 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,152 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+	 public static void main(String [] args) throws Exception { 
+	  Polynomial p = new Polynomial(); 
+	  System.out.println(p.evaluate(3)); //0.0
+	  
+	  
+	  double [] c1 = {6,5}; 
+	  int[] e1 = {0, 3};
+	  Polynomial p1 = new Polynomial(c1, e1); 
+	  double [] c2 = {3, 5, 1, 67, 3}; 
+	  int [] e2 = {4, 6, 1, 7, 3};
+	  Polynomial p2 = new Polynomial(c2, e2); 
+	  double [] c3 = {1};
+	  int [] e3 = {0};
+	  Polynomial p3 = new Polynomial(c3, e3);
+	  double [] c4 = {1};
+	  int [] e4 = {1};
+	  Polynomial p4 = new Polynomial(c4, e4);
+	  
+	  System.out.println("Base constructor tests");
+	  System.out.println(Arrays.toString(p1.coef)); //[6.0, 5.0]
+	  System.out.println(Arrays.toString(p2.exp)); //[4, 6, 1, 7, 3]
+	  System.out.println(p2.evaluate(2)); //8970.0
+	  System.out.println(p1.hasRoot(-1.063)); //false
+	  System.out.println(p4.hasRoot(0)); //true
+	  
+	  System.out.println(); //gap in output
+	  
+	  System.out.println("Addition test");
+	  Polynomial added = p1.add(p2);
+	  System.out.println(Arrays.toString(added.coef)); //[8.0, 3.0, 5.0, 1.0, 67.0, 6.0]
+	  System.out.println(Arrays.toString(added.exp)); //[3, 4, 6, 1, 7, 0] *this and ^ can be any order as long as the exp and coefs align correctly (i think)
+	  
+	  System.out.println();
+	  
+	  System.out.println("Multiplication test");
+	  Polynomial mult = p1.multiply(p2);
+	  System.out.println(Arrays.toString(mult.coef)); //[23.0, 45.0, 6.0, 417.0, 18.0, 25.0, 335.0]
+	  System.out.println(Arrays.toString(mult.exp)); //[4, 6, 1, 7, 3, 9, 10] *same note as with addition
+	  System.out.println(mult.evaluate(4)); //3.64850072E8 -> i checked this on desmos, this is true
+	  System.out.println(mult.evaluate(2)); //412620.0 i wanted a nicer number
+	  System.out.println(mult.hasRoot(0)); //true
+	  System.out.println(mult.hasRoot(1)); //false
+	  
+	  System.out.println();
+	  
+	  System.out.println("Multiplication test on 1");
+	  Polynomial mult1 = p1.multiply(p3);
+	  System.out.println(Arrays.toString(mult1.coef)); //[5.0, 6.0]
+	  System.out.println(Arrays.toString(mult1.exp)); //[3, 0] again order doesn't matter as long as they align correctly
+	  
+	  System.out.println("Multiplication test on x");
+	  Polynomial multx = p1.multiply(p4);
+	  System.out.println(Arrays.toString(multx.coef)); //[6.0, 5.0]
+	  System.out.println(Arrays.toString(multx.exp)); //[1, 4] again order doesn't matter as long as they align correctly
+	  
+	  System.out.println();
+	  
+	  System.out.println("Multiplication test on 0");
+	  Polynomial mult0 = mult.multiply(p);
+	  System.out.println(Arrays.toString(mult0.coef)); //[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+	  System.out.println(Arrays.toString(mult0.exp)); //[4, 6, 1, 7, 3, 9, 10] 
+	  //^honestly idk how we should treat this case and i guess output depends on how your method works, but at least for me i dont care enough and this is technically a 0 polynomial
+	  
+	  System.out.println();
+	  
+	  System.out.println("Addition test on x and 0");
+	  Polynomial add1 = p1.add(p4);
+	  System.out.println(Arrays.toString(add1.coef)); //[5.0, 1.0, 6.0]
+	  System.out.println(Arrays.toString(add1.exp)); //[3, 1, 0]
+	  Polynomial add0 = p1.add(p);
+	  System.out.println(Arrays.toString(add0.coef)); //[5.0, 6.0]
+	  System.out.println(Arrays.toString(add0.exp)); //[3, 0]
+
+	  mult.saveToFile("pleasework.txt");
+	  System.out.println();
+	  System.out.println("Reminder for what this should be");
+	  System.out.println(Arrays.toString(mult.coef)); //[23.0, 45.0, 6.0, 417.0, 18.0, 25.0, 335.0]
+	  System.out.println(Arrays.toString(mult.exp)); //[4, 6, 1, 7, 3, 9, 10] *same note as with addition
+	  
+	  //**************************YOU NEED TO ADD THESE FILES FOR THE NEXT 2 TESTS*****************************
+	  File file1 = new File("C:\\Users\\rzhao\\eclipse-workspace\\CSCB07\\pleasework.txt"); //change file names as needed
+	  Polynomial filed = new Polynomial(file1);
+	  System.out.println();
+	  System.out.println("Read file constructor test"); //FILE CONTAINS LINE: 23.0x4+45.0x6+6.0x1+417.0x7+18.0x3+25.0x9+335.0x10
+	  System.out.println(Arrays.toString(filed.coef)); //should be same as above reminder
+	  System.out.println(Arrays.toString(filed.exp)); //same here
+	  
+	  File file2 = new File("C:\\Users\\rzhao\\eclipse-workspace\\CSCB07\\godareyoureal.txt");
+	  Polynomial filed2 = new Polynomial(file2);
+	  System.out.println();
+	  System.out.println("Read file constructor test 2"); //FILE CONTAINS LINE: 5-3x2+7x8
+	  System.out.println(Arrays.toString(filed2.coef)); //[5.0, -3.0, 7.0]
+	  System.out.println(Arrays.toString(filed2.exp)); //[0, 2, 8]
+	  //check that the reverse holds
+	  filed2.saveToFile("unoreversecheckmategod.txt"); //5.0-3.0x2+7.0x8 or some variation
+	  
+	  System.out.println();
+	  double [] neg = {-1}; 
+	  int [] nege = {1};
+	  Polynomial bully = new Polynomial(neg, nege); 
+	  bully.saveToFile("negative.txt");
+	  System.out.println("Check file named \"negative\""); //-x -> ok i just learned we can ignore all these weird 1x or 3x1 cases so kill me cause i dealt with them, just think about it ig and see if youre output is right
+	  													  //ok file should be -1.0x1
+	  double [] c5 = {-4, -2, 1, -1, 3}; 
+	  int [] e5 = {1, 3, 5, 6, 9};
+	  Polynomial p5 = new Polynomial(c5, e5); 
+	  p5.saveToFile("funky.txt");
+	  System.out.println("Check file named \"funky\""); //-4.0x-2.0x3+x5-x6+3.0x9 -> same here. pain.
+	  													//should be -4.0x1-2.0x3+1.0x5-1.0x6+3.0x9
+	  
+	  //im suffering
+	  File file3 = new File("C:\\Users\\rzhao\\eclipse-workspace\\CSCB07\\negative.txt");
+	  Polynomial filed3 = new Polynomial(file3);
+	  System.out.println();
+	  System.out.println("Read file constructor test 3"); //file from save to file 3
+	  System.out.println(Arrays.toString(filed3.coef)); //[-1.0]
+	  System.out.println(Arrays.toString(filed3.exp)); //[1]
+	  
+	  File file4 = new File("C:\\Users\\rzhao\\eclipse-workspace\\CSCB07\\funky.txt");
+	  Polynomial filed4 = new Polynomial(file4);
+	  System.out.println();
+	  System.out.println("Read file constructor test 4"); //file from save to file 4
+	  System.out.println(Arrays.toString(filed4.coef)); //[-4.0, -2.0, 1.0, -1.0, 3.0]
+	  System.out.println(Arrays.toString(filed4.exp)); //[1, 3, 5, 6, 9]
+	  
+	  //extra tests
+	  System.out.println();
+	  double[] c6 = {2, 3};
+	  int[] e6 = {1, 0};
+	  double[] c7 = {4};
+	  int[] e7 = {2};
+	  Polynomial p6 = new Polynomial(c6, e6);
+	  Polynomial p7 = new Polynomial(c7, e7);
+	  Polynomial p6p7 = p6.multiply(p7);
+	  Polynomial p6plusp7 = p6.add(p7);
+	  p6p7.saveToFile("p6p7.txt"); //8.0x3+12.0x2 in file
+	  p6plusp7.saveToFile("p6plusp7.txt"); //2.0x1+4.0x2+3.0 in file
+	  
+	  
+	  /*
+	  Polynomial s = p1.add(p2); 
+	  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+	  if(s.hasRoot(1)) 
+	   System.out.println("1 is a root of s"); 
+	  else 
+	   System.out.println("1 is not a root of s"); */
+	 } 
+	} 
--- Polynomial.java
+++ Polynomial.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Scanner;
 public class Polynomial {
+	/*
 	double[] coef = new double[100];
 			public Polynomial()
 			{
@@ -42,5 +46,241 @@ public class Polynomial {
 				if(ans != 0)
 					return false;
 				return true;
-			}	
+			}*/
+	double[] coef;
+	int[] exp;
+	public Polynomial()
+	{
+		coef = new double[1];
+		exp = new int[1];
+		coef[0] = 0;
+		exp[0] = 0;
+	}
+	public Polynomial(double[] d, int[] e)
+	{
+		coef = new double[d.length];
+		exp = new int[d.length];
+		for(int i = 0; i < d.length; i++)
+		{
+			coef[i] = d[i];
+			exp[i] = e[i];
+		}
+	}
+	public Polynomial(File f) throws Exception
+	{
+		Scanner input = new Scanner(f);
+		String s = input.next();
+		input.close();
+		for(int i = 0; i<s.length(); i++)
+		{
+			if(s.charAt(i) == '-' && i != 0)
+			{
+				s = s.substring(0, i) + '+' + s.substring(i, s.length());
+				i++;
+			}
+		}
+		String[] arr = s.split("\\+");
+		coef = new double[arr.length];
+		exp = new int[arr.length];
+		for(int i = 0; i<arr.length; i++)
+		{
+			int x = arr[i].indexOf('x');
+			if(x!=-1)
+			{
+				if(x == 0)
+					coef[i] = 1;
+				else
+					coef[i] = Double.parseDouble(arr[i].substring(0, x));
+				if(arr[i].substring(x, arr[i].length()).length() == 1)
+					exp[i] = 1;
+				else
+					exp[i] = Integer.parseInt(arr[i].substring(x+1, arr[i].length()));
+			}
+			else
+			{
+				coef[i] = Double.parseDouble(arr[i]);
+				exp[i] = 0;
+			}
+		}		
+	}
+	
+	public Polynomial add(Polynomial p)
+	{
+		/*
+		 * alright i just realized i did a lot of stuff to 
+		 * make the arrays as small as possible for any polynomial's given size 
+		 * while doing addition and multiplication and i just realized that was 
+		 * completely unnecessary given how i defined the field however i've committed 
+		 * and it shouldn't make any differences other than visually looking better
+		 * if you print them
+		 * edit: nvm i just modified my polynomial so it isnt defaulting to an array of size 100
+		*/
+		
+		//setup and duplicate removal
+		int[] newarray = new int[exp.length+p.exp.length]; //new array with all exponents including duplicates
+		int d = 0; //counts duplicates
+		for(int i = 0; i < exp.length; i++)
+			newarray[i] = exp[i]; //adds exponents of array 1
+		for(int i = exp.length; i < exp.length+p.exp.length; i++)
+			newarray[i] = p.exp[i-exp.length];	 //exponents of array 2
+		for(int i = 0; i < exp.length; i++)
+		{
+			for(int j = 0; j < p.exp.length; j++)
+			{
+				if(exp[i] == p.exp[j])
+					d++; //how many duplicates are there
+			}
+		}
+		int[] newexp = new int[exp.length+p.exp.length-d]; //new array with size same as number of unique exponents for new polynomial exponents
+		double[] newcoef = new double[exp.length+p.exp.length-d]; //same as above but for coefficients
+		int counter = 0;
+		for(int i = 0; i < newarray.length; i++) //adds unique exponents into new exponent array
+		//never need to consider index overflow because anything that goes over should be a duplicate
+		//note: exp 0 always at the end, but since order doesn't
+		{
+			boolean check = false;
+			for(int j = 0; j < newexp.length; j++)
+			{
+				if(newarray[i] == newexp[j])
+					check = true; //if the exponent is a duplicate, this becomes true
+			}
+			if(check != true) //^so it knows not to add it to the new array
+			{
+				newexp[counter] = newarray[i];
+				counter++;
+			}
+		}
+		//actual adding
+		for(int i = 0; i < newexp.length; i++) //adds coefficients to appropriate spot, for every element in exponent array
+		{
+			for(int j = 0; j < exp.length; j++) //check og array 1 for common exponent, if there is, add the coefficient from there to the associated place from the new exponent array
+			{
+				if(exp[j] == newexp[i])
+					newcoef[i] += coef[j];
+			}
+			for(int j = 0; j < p.exp.length; j++) //same for array 2
+			{
+				if(p.exp[j] == newexp[i])
+					newcoef[i] += p.coef[j];
+			}
+		}
+		Polynomial finallydone = new Polynomial(newcoef, newexp);
+		return finallydone;
+	}
+	
+	public double evaluate(double x)
+	{
+		double ans = 0;
+		for(int i = 0; i < coef.length; i++)
+		{
+			ans += Math.pow(x, exp[i]) * coef[i];
+		}
+		return ans;
+	}
+	
+	public boolean hasRoot(double x)
+	{
+		double ans = 0;
+		for(int i = 0; i < coef.length; i++)
+		{
+			ans += Math.pow(x, exp[i]) * coef[i];
+		}
+		if(ans != 0)
+			return false;
+		return true;
+	}
+	
+	public Polynomial multiply(Polynomial p)
+	{
+		int totalsize = exp.length * p.exp.length; //how many terms are there after first expanding a polynomial multiplication
+		double[] multcoef = new double[totalsize]; //make an array of that size for coefs
+		int[] multexp = new int[totalsize]; //and exps
+		int counter = 0; //make a separate iterator
+		for(int i = 0; i<exp.length; i++)
+		{
+			for(int j = 0; j<p.exp.length; j++)
+			{
+				multcoef[counter] = coef[i]*p.coef[j]; //multiply coef of polynomial a with every element of b
+				multexp[counter] = exp[i]+p.exp[j]; //add the coefs for each
+				counter++; //make sure they go in a new slot in the new array
+			}
+		}
+		if(counter!=totalsize)
+			System.out.println("uh oh"); //if every slot in the new array isnt filled then im in pain but this is an assignment so i wont use profanity
+		//yes this next part is almost the exact same duplicate removal process as in addition, yes i was too lazy to figure out how to make a separate function for this for them to both leech from
+		int d = 0;
+		for(int i = 0; i < multexp.length-1; i++)
+		{
+			for(int j = i+1; j < multexp.length; j++)
+			{
+				if(multexp[i] == multexp[j])
+					d++; //how many duplicates are there
+			}
+		}
+		int[] finalexp = new int[multexp.length-d]; //new array with size same as number of unique exponents for new polynomial exponents
+		double[] finalcoef = new double[multexp.length-d]; //same as above but for coefficients
+		counter = 0; //need a separate iterator again
+		for(int i = 0; i < multexp.length; i++) //adds unique exponents into new exponent array
+		//never need to consider index overflow because anything that goes over should be a duplicate
+		{
+			boolean check = false;
+			for(int j = 0; j < finalexp.length; j++)
+			{
+				if(multexp[i] == finalexp[j])
+					check = true; //if the exponent is a duplicate, this becomes true
+			}
+			if(check != true) //^so it knows not to add it to the new array
+			{
+				finalexp[counter] = multexp[i];
+				counter++; //iterates separately to ensure new terms go in new slots
+			}
+		}
+		for(int i = 0; i < finalexp.length; i++) //adds coefficients to appropriate spot, for every element in exponent array
+		{
+			for(int j = 0; j < multexp.length; j++) //check og array 1 for common exponent, if there is, add the coefficient from there to the associated place from the new exponent array
+			{
+				if(multexp[j] == finalexp[i]) //wow look a duplicate
+					finalcoef[i] += multcoef[j]; //oops no more duplicate
+			}
+		}
+		Polynomial finallydone = new Polynomial(finalcoef, finalexp);
+		return finallydone;
+	}
+	
+	public void saveToFile(String s) throws Exception
+	{
+		String poly = ""; //what the printed polynomial is saved to
+		for(int i = 0; i<exp.length; i++)
+		{
+			//I just learned we can ignore these weird cases with 1 so crap, but its all here so whatever
+			if(exp[i] == 0) //case for if exponent is 0
+				poly += Double.toString(coef[i]);
+			else if(coef[i] == 0) //if coef is 0
+				poly += '0';
+			/*this stuff deals with all the 1, -1 cases, but i never added it to the file constructor, so if you uncomment this it might not work both ways yet
+			else if(coef[i] == 1 && exp[i] != 1) //if coef is 1 with any >1 exponent
+				poly += 'x' + Integer.toString(exp[i]);
+			else if(coef[i] == -1 && exp[i] != 1) //if coef is -1 with any >1 exponent
+				poly += "-x" + Integer.toString(exp[i]);
+			else if(coef[i] == 1 && exp[i] == 1) //if coef and exp are 1
+				poly += 'x';
+			else if(coef[i] == -1 && exp[i] == 1) //if coef is -1 and exp is 1o
+				poly += "-x";
+			else if(exp[i] == 1) //if exp is 1
+				poly += Double.toString(coef[i]) + 'x';
+			*/
+			else
+				poly += Double.toString(coef[i]) + 'x' + Integer.toString(exp[i]);
+			poly += '+'; //add + between each term
+		}
+		poly = poly.substring(0, poly.length()-1); //remove last + from the last term
+		for(int i = 1; i<poly.length(); i++)
+		{
+			if(poly.charAt(i) == '-')
+				poly = poly.substring(0, i-1) + poly.substring(i, poly.length()); //if theres a negative coef, remove the +
+		}
+		PrintStream output = new PrintStream(s); //print to file s
+		output.print(poly);
+		output.close();
+	}
 }
